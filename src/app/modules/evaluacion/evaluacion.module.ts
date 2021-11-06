5852ea1994bd1e58060383da1bb2cb39b3a951c1
disto-stack <juan.ocampo.2501@gmail.com>
2021-11-06 14:49:09 -0500

    Creacion de mas componentes

--- src/app/modules/evaluacion/evaluacion.module.ts
+++ src/app/modules/evaluacion/evaluacion.module.ts
@@ -2,10 +2,35 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EvaluacionRoutingModule } from './evaluacion-routing.module';
+import { CrearInvitacionEvaluarComponent } from './invitacion-evaluar/crear-invitacion-evaluar/crear-invitacion-evaluar.component';
+import { EditarInvitacionEvaluarComponent } from './invitacion-evaluar/editar-invitacion-evaluar/editar-invitacion-evaluar.component';
+import { EliminarInvitacionEvaluarComponent } from './invitacion-evaluar/eliminar-invitacion-evaluar/eliminar-invitacion-evaluar.component';
+import { ListarInvitacionEvaluarComponent } from './invitacion-evaluar/listar-invitacion-evaluar/listar-invitacion-evaluar.component';
+import { CrearResultadoEvaluacionComponent } from './resultado-evaluacion/crear-resultado-evaluacion/crear-resultado-evaluacion.component';
+import { EditarResultadoEvaluacionComponent } from './resultado-evaluacion/editar-resultado-evaluacion/editar-resultado-evaluacion.component';
+import { ListarResultadoEvaluacionComponent } from './resultado-evaluacion/listar-resultado-evaluacion/listar-resultado-evaluacion.component';
+import { EliminarResultadoEvaluacionComponent } from './resultado-evaluacion/eliminar-resultado-evaluacion/eliminar-resultado-evaluacion.component';
+import { CrearRecordatorioComponent } from './recordatorio/crear-recordatorio/crear-recordatorio.component';
+import { EditarRecordatorioComponent } from './recordatorio/editar-recordatorio/editar-recordatorio.component';
+import { EliminarRecordatorioComponent } from './recordatorio/eliminar-recordatorio/eliminar-recordatorio.component';
+import { ListarRecordatorioComponent } from './recordatorio/listar-recordatorio/listar-recordatorio.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CrearInvitacionEvaluarComponent,
+    EditarInvitacionEvaluarComponent,
+    EliminarInvitacionEvaluarComponent,
+    ListarInvitacionEvaluarComponent,
+    CrearResultadoEvaluacionComponent,
+    EditarResultadoEvaluacionComponent,
+    ListarResultadoEvaluacionComponent,
+    EliminarResultadoEvaluacionComponent,
+    CrearRecordatorioComponent,
+    EditarRecordatorioComponent,
+    EliminarRecordatorioComponent,
+    ListarRecordatorioComponent
+  ],
   imports: [
     CommonModule,
     EvaluacionRoutingModule
--- src/app/modules/solicitud/solicitud.module.ts
+++ src/app/modules/solicitud/solicitud.module.ts
@@ -2,10 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SolicitudRoutingModule } from './solicitud-routing.module';
+import { EditarSolicitudComponent } from './solicitud/editar-solicitud/editar-solicitud.component';
+import { CrearSolicitudComponent } from './solicitud/crear-solicitud/crear-solicitud.component';
+import { ListarSolicitudComponent } from './solicitud/listar-solicitud/listar-solicitud.component';
+import { EliminarSolicitudComponent } from './solicitud/eliminar-solicitud/eliminar-solicitud.component';
+import { CrearProponenteComponent } from './proponente/crear-proponente/crear-proponente.component';
+import { EditarProponenteComponent } from './proponente/editar-proponente/editar-proponente.component';
+import { ListarProponenteComponent } from './proponente/listar-proponente/listar-proponente.component';
+import { EliminarProponenteComponent } from './proponente/eliminar-proponente/eliminar-proponente.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EditarSolicitudComponent,
+    CrearSolicitudComponent,
+    ListarSolicitudComponent,
+    EliminarSolicitudComponent,
+    CrearProponenteComponent,
+    EditarProponenteComponent,
+    ListarProponenteComponent,
+    EliminarProponenteComponent
+  ],
   imports: [
     CommonModule,
     SolicitudRoutingModule
