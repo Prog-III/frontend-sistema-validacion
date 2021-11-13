c1603e5452b533aca1f4957c885e80a27d7d1d32
Viviana <leidy.1701913815@ucaldas.edu.co>
2021-11-12 19:04:54 -0500

    Enrutamiento de los componentes de Seguridad

--- src/app/modules/seguridad/seguridad-routing.module.ts
+++ src/app/modules/seguridad/seguridad-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CambioClaveComponent } from './cambio-clave/cambio-clave.component';
+import { LoginComponent } from './login/login.component';
+import { RecuperacionClaveComponent } from './recuperacion-clave/recuperacion-clave.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "cambio-clave",
+    component:CambioClaveComponent
+  },
+  {
+    path: "login",
+    component:LoginComponent
+  },
+  {
+    path: "recuperacion-clave",
+    component:RecuperacionClaveComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
