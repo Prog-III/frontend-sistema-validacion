71ad43ab1581fec4da0d0ff06707552e2c16746c
Viviana <leidy.1701913815@ucaldas.edu.co>
2021-11-12 19:11:35 -0500

    Enrutamiento de los componentes de Solicitud

--- src/app/modules/solicitud/solicitud-routing.module.ts
+++ src/app/modules/solicitud/solicitud-routing.module.ts
@@ -1,7 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearProponenteComponent } from './proponente/crear-proponente/crear-proponente.component';
+import { EditarProponenteComponent } from './proponente/editar-proponente/editar-proponente.component';
+import { EliminarProponenteComponent } from './proponente/eliminar-proponente/eliminar-proponente.component';
+import { ListarProponenteComponent } from './proponente/listar-proponente/listar-proponente.component';
+import { CrearSolicitudComponent } from './solicitud/crear-solicitud/crear-solicitud.component';
+import { EditarSolicitudComponent } from './solicitud/editar-solicitud/editar-solicitud.component';
+import { EliminarSolicitudComponent } from './solicitud/eliminar-solicitud/eliminar-solicitud.component';
+import { ListarSolicitudComponent } from './solicitud/listar-solicitud/listar-solicitud.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "crear-proponente",
+    component:CrearProponenteComponent
+  },
+  {
+    path: "editar-proponente",
+    component:EditarProponenteComponent
+  },
+  {
+    path: "eliminar-proponente",
+    component:EliminarProponenteComponent
+  },
+  {
+    path: "listar-proponente",
+    component:ListarProponenteComponent
+  },
+  {
+    path: "crear-solicitud",
+    component:CrearSolicitudComponent
+  },
+  {
+    path: "editar-solicitud",
+    component:EditarSolicitudComponent
+  },
+  {
+    path: "eliminar-solicitud",
+    component:EliminarSolicitudComponent
+  },
+  {
+    path: "listar-solicitud",
+    component:ListarSolicitudComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
