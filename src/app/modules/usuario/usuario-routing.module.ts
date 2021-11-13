54e524eacdfee6b9c6e2037df7e0931d522c310d
Viviana <leidy.1701913815@ucaldas.edu.co>
2021-11-12 19:14:13 -0500

    Enrutamiento de los componentes de Usuario

--- src/app/modules/usuario/usuario-routing.module.ts
+++ src/app/modules/usuario/usuario-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearUsuarioComponent } from './usuario/crear-usuario/crear-usuario.component';
+import { EditarUsuarioComponent } from './usuario/editar-usuario/editar-usuario.component';
+import { EliminarUsuarioComponent } from './usuario/eliminar-usuario/eliminar-usuario.component';
+import { ListarUsuarioComponent } from './usuario/listar-usuario/listar-usuario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "crear-usuario",
+    component:CrearUsuarioComponent
+  },
+  {
+    path: "editar-usuario",
+    component:EditarUsuarioComponent
+  },
+  {
+    path: "eliminar-usuario",
+    component:EliminarUsuarioComponent
+  },
+  {
+    path: "listar-usuario",
+    component:ListarUsuarioComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
