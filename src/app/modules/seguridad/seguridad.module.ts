5fca42eda67c880da27966708f79b0211f75d610
disto-stack <juan.ocampo.2501@gmail.com>
2021-11-15 13:08:18 -0500

    Primeros estilos del login

--- src/app/modules/seguridad/seguridad.module.ts
+++ src/app/modules/seguridad/seguridad.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxCaptchaModule } from 'ngx-captcha';
 
 import { SeguridadRoutingModule } from './seguridad-routing.module';
 import { LoginComponent } from './login/login.component';
@@ -15,7 +17,9 @@ import { RecuperacionClaveComponent } from './recuperacion-clave/recuperacion-cl
   ],
   imports: [
     CommonModule,
-    SeguridadRoutingModule
+    SeguridadRoutingModule,
+    ReactiveFormsModule,
+    NgxCaptchaModule
   ]
 })
 export class SeguridadModule { }
