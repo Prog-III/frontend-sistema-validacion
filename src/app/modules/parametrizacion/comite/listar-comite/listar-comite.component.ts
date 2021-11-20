9600ed9712d5eff34e8453589ef4f1e4a73fddc5
Albert Muñoz <albert.1701920351@ucaldas.edu.co>
2021-11-19 21:01:33 -0500

    Implementacion eliminar on single page

--- src/app/modules/parametrizacion/comite/listar-comite/listar-comite.component.ts
+++ src/app/modules/parametrizacion/comite/listar-comite/listar-comite.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ComiteModel } from 'src/app/models/parametros/comite.model';
 import { ComiteService } from 'src/app/servicios/parametros/comite.service';
 
@@ -12,8 +13,11 @@ export class ListarComiteComponent implements OnInit {
   recordList: ComiteModel[] = [];
 
 
+  id: number= 0;
 
   constructor(
+    private router: Router,
+    private route: ActivatedRoute,
     private service: ComiteService
   ) { }
 
@@ -27,6 +31,23 @@ export class ListarComiteComponent implements OnInit {
         this.recordList = data;
       }
     })
+  }
+
+  EliminarRegistro(id: number | undefined){
+    if(id){
+      this.service.EliminarRegistro(id).subscribe({
+        next: (data: ComiteModel) =>{
+          //aqui va el modal
+          console.log("Se elimino el mensaje");
+          location.reload();
+          
+        },
+        error: (err:any)=>{
+          //modal de error
+          console.log("No se elimino");
+        }
+      });
+    }
   }
 
 }
