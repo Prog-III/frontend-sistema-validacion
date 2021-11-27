63f4dd05b53f2233107faaa21a2f9a4a8684b55d
Viviana <leidy.1701913815@ucaldas.edu.co>
2021-11-27 14:45:00 -0500

    Creación del Proponente

---
+++ src/app/models/parametros/proponente.model.ts
@@ -0,0 +1,14 @@
+export class ProponenteModel{
+    id?: number;
+    primer_nombre?: string;
+    otros_nombres?: string;
+    primer_apellido?: string;
+    segundo_apellido?: string;
+    documento?: number;
+    fecha_nacimiento?: string;
+    email?: string;
+    celular?: string;
+    image?: string;
+    image_public_id?: string;
+    id_tipo_vinculacion?: number;
+}
